cd4261fc961566eb51c2a13e13db1492cb1f8725
UjfaA <AlexUjfa@outlook.comgit branch -agit config --global user.email AlexUjfa@outlook.com>
2020-03-23 18:10:22 +0100

    Deployed on heroku.

---
+++ src/main/java/ujfaA/springQuiz/DatabaseConfig.java
@@ -0,0 +1,24 @@
+package ujfaA.springQuiz;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+public class DatabaseConfig {
+	
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+	
+	@Bean
+	public DataSource dataSource() {
+		HikariConfig config = new HikariConfig();
+		config.setJdbcUrl(dbUrl);
+		return new HikariDataSource(config);
+	}
+}
