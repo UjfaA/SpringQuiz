e749b791508f83ef8c7109d3e5fe476a0f149150
UjfaA <AlexUjfa@outlook.comgit branch -agit config --global user.email AlexUjfa@outlook.com>
2020-03-24 22:24:28 +0100

    deployed on heroku.

--- src/main/java/ujfaA/springQuiz/DatabaseConfig.java
+++ src/main/java/ujfaA/springQuiz/DatabaseConfig.java
@@ -14,8 +14,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/*
 import org.apache.commons.dbcp.BasicDataSource;
-
+*/
 import java.net.URI;
 import java.net.URISyntaxException;
 /*
