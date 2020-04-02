191fd96422feaea9cfe8536bffdd6786d9880dcb
UjfaA <AlexUjfa@outlook.comgit branch -agit config --global user.email AlexUjfa@outlook.com>
2020-04-02 16:35:08 +0200

    Added validation of User helped by Passay library.

---
+++ src/main/java/ujfaA/springQuiz/model/validator/NameConstraintValidator.java
@@ -0,0 +1,59 @@
+package ujfaA.springQuiz.model.validator;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.passay.CharacterRule;
+import org.passay.EnglishCharacterData;
+import org.passay.IllegalCharacterRule;
+import org.passay.LengthRule;
+import org.passay.MessageResolver;
+import org.passay.PasswordData;
+import org.passay.PasswordValidator;
+import org.passay.PropertiesMessageResolver;
+import org.passay.RepeatCharacterRegexRule;
+import org.passay.RuleResult;
+
+public class NameConstraintValidator implements ConstraintValidator<ValidName, String> {
+	
+	@Override
+	public boolean isValid(String password, ConstraintValidatorContext context) {
+		
+		Properties props = new Properties();
+		try (var propertiesStream = new FileInputStream("src/main/resources/messages.properties")) {
+		props.load(propertiesStream);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		MessageResolver resolver = new PropertiesMessageResolver(props);
+		
+		PasswordValidator validator = new PasswordValidator(resolver, List.of(
+				new IllegalCharacterRule(
+						new char[]{'<','>','=',';',':','/','\\','*','+','-'},true)
+		));
+		RuleResult result = validator.validate(new PasswordData(password));
+        
+		if (result.isValid())
+            return true;
+        
+        List<String> messages = validator.getMessages(result);
+        
+        String messageTemplate = messages.stream()
+        		.collect(Collectors.joining(" "));
+        context.buildConstraintViolationWithTemplate(messageTemplate)
+        	.addConstraintViolation()
+        	.disableDefaultConstraintViolation();
+		return false;
+	}
+
+}
