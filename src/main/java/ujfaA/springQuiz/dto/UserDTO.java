76177ca02c04eb46af35d1cb2e4cd50f63e6be85
UjfaA <AlexUjfa@outlook.comgit branch -agit config --global user.email AlexUjfa@outlook.com>
2020-04-13 18:16:24 +0200

    Change what user info to display. Redact email.

--- src/main/java/ujfaA/springQuiz/dto/UserDTO.java
+++ src/main/java/ujfaA/springQuiz/dto/UserDTO.java
@@ -1,11 +1,53 @@
 package ujfaA.springQuiz.dto;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
 import lombok.Value;
+import ujfaA.springQuiz.model.Role;
 
 @Value
 public class UserDTO {
+
 	String username;
 	String email;
 	String firstName;
 	String lastName;
+	Role role;
+	LocalDateTime lastActive;
+	
+	
+	public UserDTO(String username, String email, String firstName, String lastName, Role role, LocalDateTime lastActive) {
+		
+		this.username	= username;
+		this.email		= redact(email);
+		this.firstName	= firstName;
+		this.lastName	= lastName;
+		this.role 		= role;
+		this.lastActive = lastActive;
+	}
+
+	private String redact(String emailFull) {
+		
+		StringBuilder sb = new StringBuilder();
+		String[] parts = emailFull.split("@");
+		char[] chars = new char[parts[0].length()];
+		Arrays.fill(chars, '*');
+		sb.append(chars);
+		
+		switch (chars.length) {
+		case 0:
+		case 1: break;
+		case 2: {sb.setCharAt(0, parts[0].charAt(0)); break;}
+		default: {
+			int end = parts[0].length()-1;
+			sb.setCharAt(0, parts[0].charAt(0)); 
+			sb.setCharAt(end, parts[0].charAt(end));
+			break;
+			}
+		}
+		sb.append('@').append(parts[1]);
+		return sb.toString();
+	}
+
 }
--- src/main/java/ujfaA/springQuiz/repository/UserRepository.java
+++ src/main/java/ujfaA/springQuiz/repository/UserRepository.java
@@ -18,7 +18,7 @@ public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
 	public Optional<User> findByUsername(String username);
 	
-	@Query("SELECT NEW ujfaA.springQuiz.dto.UserDTO(u.username, u.email, u.firstName, u.lastName)"
+	@Query("SELECT NEW ujfaA.springQuiz.dto.UserDTO(u.username, u.email, u.firstName, u.lastName, u.role, u.lastActive)"
 			+ " FROM User u")
 	public Set<UserDTO> getUsersInfo();
 	
